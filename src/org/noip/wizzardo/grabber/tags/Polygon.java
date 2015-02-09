ba3f4717419fb117a4e479696bdc0da93943a19a
Alexander Vlasov <vaa25@yandex.ru>
2015-02-10 00:01:14 +0200

    PolygonsDaoTest added

--- src/org/noip/wizzardo/grabber/tags/Polygon.java
+++ src/org/noip/wizzardo/grabber/tags/Polygon.java
@@ -26,4 +26,28 @@ public class Polygon {
                 ", y=" + y +
                 '}';
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Polygon)) return false;
+
+        Polygon polygon = (Polygon) o;
+
+        if (Double.compare(polygon.x, x) != 0) return false;
+        if (Double.compare(polygon.y, y) != 0) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        temp = Double.doubleToLongBits(x);
+        result = (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(y);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
 }
--- test/org/noip/wizzardo/db/tables/myObjects/PolygonsDAOTest.java
+++ test/org/noip/wizzardo/db/tables/myObjects/PolygonsDAOTest.java
@@ -4,31 +4,55 @@ import junit.framework.TestCase;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.noip.wizzardo.db.DataBase;
+import org.noip.wizzardo.grabber.tags.Polygon;
 
 public class PolygonsDAOTest extends TestCase {
+    int id;
+    private PolygonsDAO dao;
 
     @Before
     public void setUp() throws Exception {
-
+        dao = new PolygonsDAO(new DataBase().getStatement());
     }
 
     @After
     public void tearDown() throws Exception {
-
+        dao.delete(id);
     }
 
     @Test
     public void testCreate() throws Exception {
+        //given
+        Polygon polygon = new Polygon(1, 2);
 
+        //when
+        id = dao.create(polygon);
+
+        //then
+        assertEquals(polygon, dao.read(id));
     }
 
     @Test
     public void testRead() throws Exception {
+        //given
+        Polygon polygon = new Polygon(3, 2);
+        id = dao.create(polygon);
 
+        //when then
+        assertEquals(polygon, dao.read(id));
     }
 
     @Test
     public void testDelete() throws Exception {
+        //given
+        Polygon polygon = new Polygon(3, 2);
+        id = dao.create(polygon);
 
+        //when
+        dao.delete(id);
+
+        // then
+        assertNull(dao.read(id));
     }
 }
